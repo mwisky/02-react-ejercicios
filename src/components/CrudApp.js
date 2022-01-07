eaca7cecb7a96bfa644da6eed7d2689c39501373
Mauricio <mwisky@gmail.com>
2022-01-06 22:15:14 -0500

    Guardando cambios video 31

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -45,22 +45,33 @@ const CrudApp = () => {
         setDb(newData)
     }
     
-    const deleteData = (id) => {}
+    const deleteData = (id) => {
+        let isDelete = window.confirm(`¿Estás seguro de eliminar el registro con el id '${id}'`)
+        
+        if (isDelete) {
+            let newData = db.filter((el) => el.id !== id)
+            setDb(newData)
+        } else {
+            return
+        }
+    }
 
     return (
         <div>
             <h2>CRUD App</h2>
-            <CrudForm 
-                createData={createData} 
-                updateData={updateData} 
-                dataToEdit={dataToEdit} 
-                setDataToEdit={setDataToEdit}
-            />
-            <CrudTable 
-                data={db} 
-                setDataToEdit={setDataToEdit} 
-                deleteData={deleteData}
-            />
+            <article className='grid-1-2'>
+                <CrudForm 
+                    createData={createData} 
+                    updateData={updateData} 
+                    dataToEdit={dataToEdit} 
+                    setDataToEdit={setDataToEdit}
+                />
+                <CrudTable 
+                    data={db} 
+                    setDataToEdit={setDataToEdit} 
+                    deleteData={deleteData}
+                />
+            </article>
         </div>
     )
 }
