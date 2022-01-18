20527f8b38541c74b6d8066f862b5cb2c5981a97
Mauricio <mwisky@gmail.com>
2022-01-18 17:55:43 -0500

    Guardando cambios video 44

---
+++ src/components/SelectsAnidados.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import SelectList from "./SelectList"
+
+const SelectsAnidados = () => {
+    const [state, setState] = useState("")
+    const [town, setTown] = useState("")
+    const [suburb, setSuburb] = useState("")
+
+    const TOKEN = "0ede8aa5-6563-4ce2-aac2-a59d2fd6eaab"
+
+    return (
+        <div>
+            <h2>Selects Anidados</h2>
+            <h3>México</h3>
+            <SelectList 
+                title="estado" 
+                url={`https://api.copomex.com/query/get_estados?token=${TOKEN}`}
+                handleChange={(e) => setState(e.target.value)}
+            />
+            {state && <SelectList 
+                title="municipios" 
+                url={`https://api.copomex.com/query/get_municipio_por_estado/${state}?token=${TOKEN}`} 
+                handleChange={(e) => setTown(e.target.value)}
+            />}
+            {town && <SelectList 
+                title="colonia" 
+                url={`https://api.copomex.com/query/get_colonia_por_municipio/${town}?token=${TOKEN}`} 
+                handleChange={(e) => setSuburb(e.target.value)}
+            />}
+            <pre>
+                <code>
+                    {state} - {town} - {suburb}
+                </code>
+            </pre>
+        </div>
+    )
+}
+
+export default SelectsAnidados
