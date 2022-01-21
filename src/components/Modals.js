f78729f9e5a28fe4e5a8ac7310cc7ed8a7ddc150
Mauricio <mwisky@gmail.com>
2022-01-21 17:33:46 -0500

    Guardando cambios hasta video 51

---
+++ src/components/Modals.js
@@ -0,0 +1,31 @@
+import { useModal } from "../hooks/useModal";
+import ContactForm from "./ContactForm";
+import Modal from "./Modal";
+
+const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+  return (
+  <div>
+      <h2>Modales</h2>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+          <h3>Modal 1</h3>
+          <p>Hola este es el contenido de mi Modal 1</p>
+          <img src="https://placeimg.com/400/400/animals" alt="Animals" />
+      </Modal>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+          <h3>Otro Modal</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere possimus quos sint repellendus, minima quibusdam impedit. Excepturi, quam! Ducimus, sit similique at doloribus facilis rem quibusdam corrupti vero quae accusamus.</p>
+          <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+      </Modal>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+          <ContactForm/>
+      </Modal>
+  </div>
+)};
+
+export default Modals;
